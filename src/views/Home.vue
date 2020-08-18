22d3bd5f12bafcb0c8f743050421e74ea6edb592
NeoWu <wuhongyi2011@gmail.com>
2020-08-18 21:44:10 +0800

    引入element ui 前儲存

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <!--  <div class="home">-->
+    <!--    <img alt="Vue logo" src="../assets/logo.png">-->
+    <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <el-container class="home-page-container">
+        <el-header class="main-page-header">
+
+        </el-header>
+    </el-container>
+
+    <!--  </div>-->
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    // import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home',
+        components: {
+            // HelloWorld
+        }
+    }
 </script>
+<style lang="scss">
+    * {
+        margin: 0;
+        height: 100vh;
+    }
+
+
+
+    .main-page-header {
+        width: 100vw;
+        height: 30px;
+        background-color: red;
+        text-align: right;
+    }
+</style>
