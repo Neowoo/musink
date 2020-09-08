0856b0b45fc86e912f7889abaae98d4733e0f099
NeoWu <wuhongyi2011@gmail.com>
2020-09-08 23:58:24 +0800

    add variable

--- src/views/RecommendFile.vue
+++ src/views/RecommendFile.vue
@@ -35,6 +35,7 @@
     export default {
         data() {
             return {
+                testFeature: '',
                 videoInfos: [
                     {
                         url: defaultVideoImg,
